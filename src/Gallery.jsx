082eac1b2e94f9f4ba668c03c7521a17725491df
Doston21-wq <rajabovdoston500@gmail.com>
2025-05-28 08:27:35 +0500

    addd

--- src/Gallery.jsx
+++ src/Gallery.jsx
@@ -1,6 +1,30 @@
+import { useQuery } from '@tanstack/react-query'
+import axios from 'axios'
 import React from 'react'
-
+const url='https://api.unsplash.com/search/photos?client_id=xH2dss2cIxvpVJ1yiprAgYw0DWIcu3JBtgqVuVT4iaM&query=cat'
 const Gallery = () => {
+  const {data, isError, isLoading} = useQuery({
+    queryKey: ['images'],
+    queryFn: async () => {
+      const results = axios.get(url)
+      return (await results).data
+    }
+  })
+  if(isLoading){
+    return 
+    <section className='Image-contanier'>
+      <h1>loading...</h1>
+    </section>
+  }
+
+if(isError){
+    return 
+    <section className='Image-contanier'>
+      <h1>Error...</h1>
+    </section>
+  }
+
+  
   return (
     <div>Gallery</div>
   )
--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,9 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import {AppProvider} from './context'
-import { QueryClientProvider } from '@tanstack/react-query'
-const queryClient = new QueryClientProvider()
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
 createRoot(document.getElementById('root')).render(
   <AppProvider>
     <QueryClientProvider client={queryClient}>  
